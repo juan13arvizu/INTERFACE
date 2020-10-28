377f0fa462021cfde7f366065536c918576f87e8
juan13arvizu <71351101+juan13arvizu@users.noreply.github.com>
2020-10-28 14:22:03 -0600

    Design of User Interface
    All good

---
+++ USER/LoginForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace USER
+{
+    partial class Background
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Background));
+            this.Access_Button = new System.Windows.Forms.Button();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.View_check = new System.Windows.Forms.CheckBox();
+            this.PswtxtBox = new System.Windows.Forms.TextBox();
+            this.Password_Label = new System.Windows.Forms.Label();
+            this.UsertxtBox = new System.Windows.Forms.TextBox();
+            this.Username_Label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Access_Button
+            // 
+            this.Access_Button.AccessibleRole = System.Windows.Forms.AccessibleRole.Sound;
+            this.Access_Button.BackColor = System.Drawing.Color.DarkViolet;
+            this.Access_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Access_Button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Access_Button.FlatAppearance.BorderSize = 0;
+            this.Access_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.Access_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Access_Button.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Access_Button.ForeColor = System.Drawing.Color.GhostWhite;
+            this.Access_Button.Location = new System.Drawing.Point(441, 311);
+            this.Access_Button.Name = "Access_Button";
+            this.Access_Button.Size = new System.Drawing.Size(60, 43);
+            this.Access_Button.TabIndex = 25;
+            this.Access_Button.Text = "GO";
+            this.Access_Button.UseVisualStyleBackColor = false;
+            this.Access_Button.UseWaitCursor = true;
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.BackColor = System.Drawing.Color.Honeydew;
+            this.RegisterButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.RegisterButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RegisterButton.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterButton.ForeColor = System.Drawing.Color.DarkViolet;
+            this.RegisterButton.Location = new System.Drawing.Point(218, 311);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(135, 31);
+            this.RegisterButton.TabIndex = 24;
+            this.RegisterButton.Text = "REGISTER";
+            this.RegisterButton.UseVisualStyleBackColor = false;
+            // 
+            // View_check
+            // 
+            this.View_check.AutoSize = true;
+            this.View_check.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.View_check.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.View_check.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.View_check.Location = new System.Drawing.Point(349, 252);
+            this.View_check.Name = "View_check";
+            this.View_check.Size = new System.Drawing.Size(58, 19);
+            this.View_check.TabIndex = 23;
+            this.View_check.Text = "view";
+            this.View_check.UseVisualStyleBackColor = false;
+            // 
+            // PswtxtBox
+            // 
+            this.PswtxtBox.BackColor = System.Drawing.Color.Black;
+            this.PswtxtBox.Font = new System.Drawing.Font("Symbol", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.PswtxtBox.ForeColor = System.Drawing.Color.LightBlue;
+            this.PswtxtBox.Location = new System.Drawing.Point(219, 246);
+            this.PswtxtBox.Name = "PswtxtBox";
+            this.PswtxtBox.Size = new System.Drawing.Size(124, 31);
+            this.PswtxtBox.TabIndex = 22;
+            // 
+            // Password_Label
+            // 
+            this.Password_Label.AutoSize = true;
+            this.Password_Label.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.Password_Label.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password_Label.ForeColor = System.Drawing.Color.Black;
+            this.Password_Label.Location = new System.Drawing.Point(214, 198);
+            this.Password_Label.Name = "Password_Label";
+            this.Password_Label.Size = new System.Drawing.Size(143, 30);
+            this.Password_Label.TabIndex = 21;
+            this.Password_Label.Text = "Password";
+            // 
+            // UsertxtBox
+            // 
+            this.UsertxtBox.BackColor = System.Drawing.Color.Black;
+            this.UsertxtBox.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsertxtBox.ForeColor = System.Drawing.Color.Gold;
+            this.UsertxtBox.Location = new System.Drawing.Point(212, 144);
+            this.UsertxtBox.Name = "UsertxtBox";
+            this.UsertxtBox.Size = new System.Drawing.Size(138, 30);
+            this.UsertxtBox.TabIndex = 20;
+            // 
+            // Username_Label
+            // 
+            this.Username_Label.AutoSize = true;
+            this.Username_Label.BackColor = System.Drawing.Color.Yellow;
+            this.Username_Label.Font = new System.Drawing.Font("Showcard Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username_Label.ForeColor = System.Drawing.Color.Black;
+            this.Username_Label.Location = new System.Drawing.Point(200, 93);
+            this.Username_Label.Name = "Username_Label";
+            this.Username_Label.Size = new System.Drawing.Size(173, 36);
+            this.Username_Label.TabIndex = 19;
+            this.Username_Label.Text = "Username";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkViolet;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(203, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 60);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "LOGIN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Background
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Access_Button);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.View_check);
+            this.Controls.Add(this.PswtxtBox);
+            this.Controls.Add(this.Password_Label);
+            this.Controls.Add(this.UsertxtBox);
+            this.Controls.Add(this.Username_Label);
+            this.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Background";
+            this.Text = "LOGIN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Access_Button;
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.CheckBox View_check;
+        private System.Windows.Forms.TextBox PswtxtBox;
+        private System.Windows.Forms.Label Password_Label;
+        private System.Windows.Forms.TextBox UsertxtBox;
+        private System.Windows.Forms.Label Username_Label;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ USER/LoginForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace USER
+{
+    public partial class Background : Form
+    {
+        public Background()
+        {
+            InitializeComponent();
+        }
+        private void Login()
+        {
+            string Connect = "datasource=localhost;port=3306;username=root;password=;database=user";
+            string query = "SELECT * FROM user where Username = '" + UsertxtBox.Text + "'AND Password='" + PswtxtBox.Text + "'";
+            MySqlConnection databaseConnection = new MySqlConnection(Connect);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader bibliophile;
+
+
+            try
+            {
+                databaseConnection.Open();
+                bibliophile = commandDatabase.ExecuteReader();
+                if (bibliophile.Read())
+                {
+                    MessageBox.Show("ACCESS ACCEPTED");
+                }
+                else
+                {
+                    MessageBox.Show("Username/Password, MISTAKEN.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Register()
+        {
+            string Connect = "datasource=localhost;port=3306;username=root;password=;database=user";
+            string query = "INSERT * INTO user (`id`,`Username`, `Password`) VALUES (NULL, '" + UsertxtBox.Text + "','" + PswtxtBox.Text + "')";
+            MySqlConnection databaseConnection = new MySqlConnection(Connect);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader bibliophile;
+
+
+            try
+            {
+                databaseConnection.Open();
+                bibliophile = commandDatabase.ExecuteReader();
+                if (bibliophile.Read())
+                {
+                    MessageBox.Show("SUCCESSFUL REGISTARING");
+                }
+                else
+                {
+                    MessageBox.Show("");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Access_Button_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+        private void UsertxtBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PswtxtBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (UsertxtBox.Text == "")
+                {
+                    MessageBox.Show("Username is missing");
+
+                }
+                else if (PswtxtBox.Text == "")
+                {
+                    MessageBox.Show("Password is missing");
+
+                }
+                else if (UsertxtBox.Text == "" && PswtxtBox.Text == "")
+                {
+                    MessageBox.Show("Come on, both are missing!");
+
+                }
+                else
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+        private void Save_Botton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (UsertxtBox.Text == "")
+                {
+                    MessageBox.Show("Username is missing");
+
+                }
+                else if (PswtxtBox.Text == "")
+                {
+                    MessageBox.Show("Password is missing");
+
+                }
+                else if (UsertxtBox.Text == "" && PswtxtBox.Text == "")
+                {
+                    MessageBox.Show("Come on, both are missing!");
+
+                }
+                else
+                {
+                    Register();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        private void RegisterButton__Click(object sender, EventArgs e)
+        {
+            Register registered = new Register();
+            registered.Show();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+ 
